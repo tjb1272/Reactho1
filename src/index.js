587ebfa76639fc63e2e59d58b7fae75ae146c38c
tjb1272 <tjb1272@gmail.com>
2017-11-19 17:15:57 -0700

    react2

--- src/index.js
+++ src/index.js
@@ -1,80 +1,83 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-/* Hands-On Project 1*/
-class NameTable extends React.Component {
-    render() {
-        return (
-            <table>
-                <tbody>
-                <tr>
-                    <th>{this.props.FN}</th>
-                    <th>{this.props.LN}</th>
-                </tr>
-                <tr>
-                    <td>{this.props.FN2}</td>
-                    <td>{this.props.LN2}</td>
-                </tr>
-                <tr>
-                    <td>{this.props.FN3}</td>
-                    <td>{this.props.LN3}</td>
-                </tr>
-                </tbody>
-            </table>  
-        )
-    }
-}
+/* Hands-On Project 1 -- JSX & Stateless Components*/
 
-ReactDom.render (
-    <NameTable FN="First Name" LN="Last Name" FN2="Janet" LN2="James" FN3="John" LN3="Jameson"  />,
-        document.getElementById('root')
-)
+// class NewConcepts extends React.Component {
+// render () {
+//   return (
+//     <div>
+//       <h1>Tips for breaking down a new concept</h1>
+//         <div>
+//           <ul>
+//             <li>{this.props.concepts1}</li>
+//           <ul>
+//             <li>{this.props.concepts2}</li>
+//             <li>{this.props.concepts3}</li>
+//           </ul>
+//           </ul>
+//           <ul>
+//             <li>{this.props.concepts4}</li>
+//           <ul>
+//             <li>{this.props.concepts5}</li>
+//             <li>{this.props.concepts6}</li>
+//           </ul>
+//         </ul>
+//       </div>
+//     </div>
+//     )
+//   }
+// }
+
+// ReactDom.render(
+//     <NewConcepts    concepts1 = "Many concepts can't be tackled all at once" 
+//                     concepts2 = "Work with the parts you can understand" 
+//                     concepts3 = "Take breaks to stay fresh" 
+//                     concepts4 = "Develop a plan to learn the parts you don't know"
+//                     concepts5 = "Start with the parts that you know" 
+//                     concepts6 = "Build new concepts on top" />, 
+//         document.getElementById('root'));
+  
+
+/* Hands-On Project 2 (Optional) -- JSX & Stateless Components*/
 
 
-/* Hands-On Project 2*/
-const hasUsernameAndPassword = (user) => {
-    return user.name.length > 0 && user.password.length > 0;
-}
-
-const filterInvalidUsers = (users) => {
-const validUsers = [];
-
-    for (let user of users) {
-        if (hasUsernameAndPassword(user)) {
-            validUsers.push(user);
-        }
-    }
-    return validUsers;
-}
-
-const userToFormattedString = (user) => {
-    return `name: ${user.name} \nrole: ${user.role}`
-}
-
-let users = [
-    {
-        name: 'Kirk Douglass',
-        password: 'iamspartacus',
-        role: 'revolutionary'
-    },
-    {
-        name: 'Charlie Chaplain',
-        password: '',
-        role: 'tramp'
-    },
-    {
-        name: 'Harrison Ford',
-        password: 'AnythingButSnakes',
-        role: 'archaeologist'
-    }
+let key = 0;
+const libraries = [
+  {
+    title: 'React',
+    description: 'A JavaScript library for building user interfaces'
+  },
+  {
+    title: 'Redux',
+    description: 'A predictable state container for JavaScript apps'
+  },
+  {
+    title: 'Reselect',
+    description: 'A simple selector library for Redux'
+  }
 ];
+const makeDescription = libraries => (
+    <div key={key++}>
+    <span>{libraries.title}: {libraries.description}</span>
+    </div>
 
-let validUsers = filterInvalidUsers(users);
+);
 
-for (let i in validUsers) {
-    console.log(userToFormattedString(validUsers[i]));
-}
+const output = libraries.map(makeDescription);
 
+ReactDom.render(
+  <div>{output}</div>,
+    document.getElementById('root'));
+
+
+
+    /* Hands-On Project 3 (Optional) -- JSX & Stateless Components*/
+// const header = makeHeader({ id: 'title', text: "Major General's Song" });
+// const author = makeBody({ author: 'Gilbert and Sullivan', year: 1879 });
+// const majorGeneral = makeSong(header, lyrics);
+    
+// ReactDom.render(output, document.getElementById('root'));    
 
 
 
